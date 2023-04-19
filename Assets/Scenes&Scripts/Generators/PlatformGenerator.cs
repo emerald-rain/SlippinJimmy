c8fc661d1efbf6b6d13baa1d4fb11eff8efe88a0
malysh-ka <52384128+emerald-rain@users.noreply.github.com>
2023-04-19 20:46:33 +0100

    Redesign input data for generator
    NOT THE FINAL VERSION. The script itself is useless

--- Assets/Scenes&Scripts/Generators/PlatformGenerator.cs
+++ Assets/Scenes&Scripts/Generators/PlatformGenerator.cs
@@ -1,132 +1,21 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class PlatformGenerator : MonoBehaviour
 {
-    public GameObject player;
-
     [System.Serializable]
-    public struct PlatformInfo
+    public struct PrebuffSettings
     {
-        public GameObject prefab;
-        public float chance;
+        public float levelY; // Level Y after which prebuffs will be generated
+        public PrebuffData[] prebuffData; // Array of prebuff data for each difficulty level
     }
 
     [System.Serializable]
-    public struct PlatformDistanceChange
+    public struct PrebuffData
     {
-        public float yPosition;
-        public float newPlatformDistance;
+        public GameObject prebuffPrefab; // Prebuff prefab
+        public float chance; // Chance of generating the prebuff
     }
 
-    public List<PlatformInfo> platforms;
-    public List<PlatformDistanceChange> platformDistanceChanges;
-    public int poolSize = 50;
-    public float levelWidth = 5f;
-
-    private Queue<GameObject> platformPool;
-    private List<GameObject> activePlatforms;
-    private float platformDistance = 1f;
-    private float minYOffset = 30f;
-    private float maxYOffset = 30f;
-    private float initialPlatformY = -10f;
-    private float lastPlatformY;
-
-    void Start()
-    {
-        lastPlatformY = initialPlatformY;
-        CreatePlatformPool();
-        FillPlatformPool();
-        activePlatforms = new List<GameObject>();
-    }
-
-    void Update()
-    {
-        float playerY = player.transform.position.y;
-
-        // Update platformDistance based on Y position changes
-        foreach (PlatformDistanceChange distanceChange in platformDistanceChanges)
-        {
-            if (playerY >= distanceChange.yPosition)
-            {
-                platformDistance = distanceChange.newPlatformDistance;
-            }
-        }
-
-        // Generate platforms above 'Y character plus 50'
-        while (lastPlatformY < playerY + maxYOffset)
-        {
-            GeneratePlatform();
-        }
-
-        // Remove platforms below 'Y character minus 20'
-        for (int i = 0; i < activePlatforms.Count; i++)
-        {
-            if (activePlatforms[i].transform.position.y < playerY - minYOffset)
-            {
-                RemovePlatform(i);
-            }
-        }
-    }
-
-    void CreatePlatformPool()
-    {
-        platformPool = new Queue<GameObject>();
-    }
-
-    void FillPlatformPool()
-    {
-        for (int i = 0; i < poolSize; i++)
-        {
-            GameObject platform = Instantiate(ChoosePlatform(), Vector3.zero, Quaternion.identity);
-            platform.SetActive(false);
-            platformPool.Enqueue(platform);
-        }
-    }
-
-    void GeneratePlatform()
-    {
-        float x = Random.Range(-levelWidth / 2f, levelWidth / 2f);
-        float y = lastPlatformY + platformDistance;
-        Vector3 position = new Vector3(x, y, 0f);
-
-        GameObject platform = platformPool.Dequeue();
-        platform.transform.position = position;
-        platform.SetActive(true);
-
-        lastPlatformY = y;
-        activePlatforms.Add(platform);
-    }
-
-    void RemovePlatform(int index)
-    {
-        GameObject platform = activePlatforms[index];
-        platform.SetActive(false);
-        platformPool.Enqueue(platform);
-        activePlatforms.RemoveAt(index);
-    }
-
-    GameObject ChoosePlatform()
-    {
-        float totalChance = 0f;
-        foreach (PlatformInfo platform in platforms)
-        {
-            totalChance += platform.chance;
-        }
-
-        float randomValue = Random.Range(0f, totalChance);
-        float currentChance = 0f;
-
-        foreach (PlatformInfo platform in platforms)
-        {
-            currentChance += platform.chance;
-            if (randomValue <= currentChance)
-            {
-                return platform.prefab;
-            }
-        }
-
-        return platforms[0].prefab;
-    }
+    public PrebuffSettings[] settings; // Public struct for the prebuff settings
+    
 }
